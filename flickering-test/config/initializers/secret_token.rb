176b42915e648b133efb739a16fead24f1fe8161
Andrey Tarantsov <andreyvit@gmail.com>
2012-04-12 03:38:38 +0700

    Rails 3 flickering CSS test

---
+++ flickering-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivereloadTest::Application.config.secret_token = 'e970339c647793cd105d2f5f24c1888e0764baf4d8c04427b2080d41652942bab8f1f638e111a72158dc25aa9fca2d793e0d6bed86db0ae76009010342fb3abd'
